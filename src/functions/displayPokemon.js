3639af61ff3236898704e1a794df3ad069164682
Christian.B <Christian.Buaka@hotmail.com>
2024-02-01 19:44:26 +0100

    Compiled from ts to js

---
+++ src/functions/displayPokemon.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.resultsContainer = exports.displayPokemon = void 0;
+var displayPokemon = function (pokemonList) {
+    var resultsContainer = document.getElementById('searchResults');
+    if (resultsContainer) {
+        resultsContainer.innerHTML = '';
+        pokemonList.forEach(function (pokemon) {
+            var pokemonImage = document.createElement('img');
+            pokemonImage.className = 'pokemonImg';
+            pokemonImage.src = pokemon.images.small;
+            pokemonImage.alt = 'Picture of Pokemon';
+            pokemonImage.addEventListener('click', function () {
+                pokemonImage.classList.add('holoEffect');
+                setTimeout(function () {
+                    pokemonImage.classList.remove('holoEffect');
+                }, 1000);
+            });
+            resultsContainer.appendChild(pokemonImage);
+        });
+    }
+};
+exports.displayPokemon = displayPokemon;
+exports.resultsContainer = document.getElementById('searchResults');
