cf7c5abb06f7fccdcaf36774a03ec70d75eb780c
Christian.B <Christian.Buaka@hotmail.com>
2024-02-01 20:59:32 +0100

    Added axion in html

--- src/main.js
+++ src/main.js
@@ -35,7 +35,6 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-import axios from "./node_modules/axios/dist/axios.min.mjs";
 
 import { displayPokemon } from "../src/functions/displayPokemon";
 var searchForm = document.getElementById('searchForm');
