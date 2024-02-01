34d58d90865d7936378f70abc8547b936e240b6d
Christian.B <Christian.Buaka@hotmail.com>
2024-02-01 19:51:10 +0100

    Chaned src to main js in script

--- src/functions/displayPokemon.js
+++ src/functions/displayPokemon.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+// Object.defineProperty(exports, "__esModule", { value: true });
 exports.resultsContainer = exports.displayPokemon = void 0;
 var displayPokemon = function (pokemonList) {
     var resultsContainer = document.getElementById('searchResults');
